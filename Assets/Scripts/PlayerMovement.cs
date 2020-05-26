277cb5547e3ba003966130122e36663f185c657a
Matthew Timm <mtimm1984@cs.unm.edu>
2020-05-26 07:46:52 -0600

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float speed = 0;
+    Animator animator;
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Awake(){
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        rb.velocity = Vector2.right * speed;
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A)){
+            speed = -5f;
+            sprite.flipX = false;
+        }else if(Input.GetKey(KeyCode.D)){
+            speed = 5f;
+            sprite.flipX = true;
+        }else{
+            speed = 0;
+        }
+        
+    }
+}
