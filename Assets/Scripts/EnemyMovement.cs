159f1e4ea23ef79a6a423512aec96766b40a5af5
Matthew Timm <mtimm1984@cs.unm.edu>
2020-05-27 09:23:32 -0600

    added a enemy movement function, and actually saved it

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    float totalMovement = 0.0f;
+    float MOVEMENTCAP = 10.0f;
+    int direction = 0;
+    float speed = 3f;
+
+    Rigidbody2D rb;
+
+    void Awake(){
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    } 
     void Start()
     {
-        
+        direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate(){
+        if(totalMovement > MOVEMENTCAP){
+            direction = -direction;
+            totalMovement = 0.0f;
+        }
+        totalMovement = Mathf.Abs(Time.deltaTime * rb.velocity.x) ;
+        rb.velocity = new Vector2(direction * speed, 0f);
     }
 }
