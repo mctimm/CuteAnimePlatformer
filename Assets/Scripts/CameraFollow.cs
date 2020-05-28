b69fa0d5548532c1e19ffadbdea7addd83166559
Matthew Timm <mtimm1984@cs.unm.edu>
2020-05-28 07:57:15 -0600

    Made the follow the player with an offset.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float offset = 5f;
+
+    Transform playerTransform;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = gameObject.transform.position;
+
+        float playerX = playerTransform.position.x;
+
+        //this should move the camera to keep up with the player
+        if(playerX < temp.x)
+        {//for if the player is going back
+            if(temp.x - playerX >= offset)
+            {
+                temp.x = playerX + offset;
+            }
+        }else if(playerX > temp.x)
+        {// for when the player is going forward.
+            if(playerX - temp.x >= offset)
+            {
+                temp.x = playerX - offset;
+            }
+        }
+
+        gameObject.transform.position = temp;
+
+    }
+}
