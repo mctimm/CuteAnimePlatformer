0bbc972e259c9d68b7ecc244abf64eadf46be481
Matthew Timm <mtimm1984@cs.unm.edu>
2020-06-01 09:22:06 -0600

    fixed a bug with ground collision& enemy spawning

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    public Transform enemyPosition;
+    BoxCollider2D hitbox;
+
+    void Awake(){
+        hitbox = gameObject.GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D col){
+        if(col.gameObject.tag.Equals("Player")){
+            Instantiate(enemy, enemyPosition.position, Quaternion.identity);
+            hitbox.enabled = false;
+
+        }
+    }
+}
