02d19e638cd0124de7d4e4b542f5b02a45aa7448
Matthew Timm <mtimm1984@cs.unm.edu>
2020-06-02 10:28:04 -0600

    working on spell casting
    made an animation for the spell casting, a fire ball sprite,
    the ability to spawn a fireball.
    
    Next: need to add animation to character, add fire ball logic, and add
    limit to casting

---
+++ Assets/Scripts/FireBallCast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallCast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject fireball;
+    public Transform firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K)){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        Instantiate(fireball, firePoint.position, Quaternion.identity);
+    }
+
+    
+}
